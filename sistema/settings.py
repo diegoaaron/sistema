87430b1a912a0a4325f13c354d2400079cb353e5
Diego Aarón <diegoaarondv@gmail.com>
2022-03-16 21:11:59 -0500

    update final

--- sistema/settings.py
+++ sistema/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/imagenes/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
